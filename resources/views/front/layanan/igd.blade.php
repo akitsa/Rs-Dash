2928ecb01e529c0b5864acf9015ef4cee779fd22
bima prihartanto <guccmvu@gmail.com>
2023-06-14 13:01:31 +0700

    editblade

--- resources/views/front/layanan/igd.blade.php
+++ resources/views/front/layanan/igd.blade.php
@@ -6,7 +6,8 @@
     </div>
 
     <div class="title">
-        <h1>Layanan Igd 24 JAM</h1>
+        <h1>Layanan Igd 24 Jam</h1>
+        <hr>
     </div>
     
     <div class="content">
@@ -14,7 +15,7 @@
             Di IGD dapat ditemukan dokter dari berbagai spesialisasi bersama sejumlah perawat dan juga asisten dokter.
             Pelayanan Gawat Darurat (Emergency care) adalah bagian dari pelayanan dari kedokteran yang dibutuhkan oleh penderita dalam waktu segera (immediately) untuk menyelamatkan kehidupan (life saving)
         
-            IGD RSU 'Aisyiyah Ponorogo terletak tepat di pinggir jalan Jl. Mayjen Sungkono No.38, RW.40, Nambangan Lor, Kec. Manguharjo, Kota Madiun, yang sangat mudah dijangkau
+            IGD RSI Siti Aisyah Madiun terletak tepat di pinggir jalan Jl. Mayjen Sungkono No.38, RW.40, Nambangan Lor, Kec. Manguharjo, Kota Madiun, yang sangat mudah dijangkau
             Silahkan klik tombol berikut ini untuk memanggil nomor Ambulance Gawat Darurat IGD Rsi Siti Aiysah Madiun</p>
     </div>
     
--- resources/views/front/layanan/jalan.blade.php
+++ resources/views/front/layanan/jalan.blade.php
@@ -7,10 +7,51 @@
 
     <div class="title">
         <h1>Layanan Rawat Jalan</h1>
+        <hr>
     </div>
 
     <div class="content">
         <p>Rawat jalan adalah pelayanan medis kepada seorang pasien untuk tujuan pengamatan, diagnosis, pengobatan, rehabilitasi, dan pelayanan kesehatan lainnya, tanpa mengharuskan pasien tersebut dirawat inap. Keuntungannya, pasien tidak perlu mengeluarkan biaya untuk menginap (opname).
             Dalam rangka meningkatkan mutu dan pelayanan kepada pasien, maka Rsi Siti Aisyah Madiun menghadirkan pelayanan klinik spesialis dan non spesialis sebagai berikut:</p>
+            <hr>   
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <p>KLINIK SPESIALIS</p>
+                <hr>
+                <p>01. Spesialis Anak</p>
+                <p>02. Spesialis Anestesi</p>
+                <p>03. Spesialis Bedah</p>
+                <p>04. Spesialis Bedah Ortopaedi</p>
+                <p>05. Spesialis Dermatologi Dan Venerologi</p>
+                <p>06. Spesialis Jantung dan Pembuluh Darah</p>
+                <p>07. Spesialis Kedokteran Jiwa</p>
+                <p>08. Spesialis Konservasi Gigi</p>
+                <p>09. Spesialis Mata</p>
+                <p>10. Spesialis Paru</p>
+                <p>11. Spesialis Penyakit Dalam</p>
+                <p>12. Spesialis THT</p>
+            </div>
+            <div class="col-sm">
+                <p>KLINIK NON SPESIALIS</p>
+                <hr>
+            </div>
+            <div class="col-sm">
+                <p>INSTALASI PENUNJANG</p>
+                <hr>
+                <p>01.Layanan Antar obat sampai rumah gratis</p>
+                <p>02.Instalasi Farmasi 24 Jam</p>
+                <p>03.Instalasi Radiologi 24Jam, Melayani RDI(Radiologi Diagnosik Imaging) dan CT Scan 16 slice</p>
+                <p>04.Instalasi Laboratorium 24 jam dan Bank Darah RS 24 Jam serta Medical Check Up </p>
+                <p>05.Instalasi Gizi</p>
+                <p>06.Instalasi Bedah Sentral</p>
+                <p>07.Laparaskopi</p>
+                <p>08.Eswl</p>
+                <p>09.One Day Care(ODC)</p>
+                <p>10.Senam Hamil & Pijat Bayi</p>
+                <p>11.Home Care</p>
+            </div>
+        </div>
     </div>
 @endsection
